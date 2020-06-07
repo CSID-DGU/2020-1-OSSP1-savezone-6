597cf822db8a697981b230c5cfd07129826daf07
Ilyong Jung <57821585+jiy12345@users.noreply.github.com>
2020-06-07 23:50:48 +0900

    Update imageClassifier.java

--- app/src/main/java/com/teamsavezone/smartcart/imageClassifier.java
+++ app/src/main/java/com/teamsavezone/smartcart/imageClassifier.java
@@ -13,7 +13,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
-package com.example.android.tflitecamerademo;
+package com.teamsavezone.smartcart;
 
 import android.app.Activity;
 import android.content.res.AssetFileDescriptor;
